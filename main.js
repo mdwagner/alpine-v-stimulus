7bd3c8c1d7e666e1a8ceca7f5606ac7a6f217fa1
Michael Wagner <mdwranger@gmail.com>
2022-02-04 01:02:22 -0500

    feat: wip some examples

--- main.js
+++ main.js
@@ -1,6 +1,60 @@
 import "./style.css";
 import Alpine from "alpinejs";
+import { Application, Controller } from "@hotwired/stimulus";
 
-window.Alpine = Alpine;
+Object.assign(window, {
+  Stimulus: Application.start(),
+  Alpine,
+});
+
+Stimulus.register(
+  "hello",
+  class extends Controller {
+    static targets = ["name"];
+    greet() {
+      console.log(`Hello, ${this.name}`);
+    }
+    get name() {
+      return this.nameTarget.value;
+    }
+  }
+);
+
+Stimulus.register(
+  "clipboard",
+  class extends Controller {
+    static targets = ["source"];
+    copy($event) {
+      //navigator.clipboard.writeText(this.sourceTarget.value);
+      $event.preventDefault();
+      this.sourceTarget.select();
+      document.execCommnd("copy");
+    }
+  }
+);
+
+Alpine.magic("clipboard", () => (subject) => {
+  navigator.clipboard.writeText(subject);
+});
+
+// Alternative approach
+Alpine.magic("rawClipboard", () => (ref) => {
+  ref.select();
+  document.execCommand("copy");
+});
+
+Alpine.data("hello", () => ({
+  name: "",
+  greet() {
+    console.log(`Hello, ${this.name}`);
+  },
+}));
+
+Alpine.data("clipboard", () => ({
+  copy() {
+    this.$refs.clipboard.select();
+    document.execCommand("copy");
+  },
+}));
 
 Alpine.start();
